2392ea40bfb34f43f250958fc36cca423e86ea72
select766 <select766@outlook.jp>
2020-08-06 20:27:20 +0900

    パーティ情報をURLに埋め込む

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,26 @@ import './App.css';
 import { MyParty, EmptyMyParty, OpponentParty, EmptyOpponentParty } from './model';
 import { MyPartyEditor } from './MyPartyEditor';
 import { OpponentPartyEditor } from './OpponentPartyEditor';
+import { useLocation, useHistory } from 'react-router-dom';
 
 function App() {
-  const [myParty, setMyParty] = React.useState<MyParty>(EmptyMyParty);
-  const [opponentParty, setOpponentParty] = React.useState<OpponentParty>(EmptyOpponentParty);
+  const location = useLocation();
+  const history = useHistory();
+
+  const urlSearchParams = new URLSearchParams(location.search);
+  const partyJSON = urlSearchParams.get('party');
+  const party: { myParty: MyParty, opponentParty: OpponentParty } = partyJSON ? JSON.parse(partyJSON) : { myParty: EmptyMyParty, opponentParty: EmptyOpponentParty };
+  const setMyParty = (newMyParty: MyParty) => {
+    history.push(`/?party=${JSON.stringify({ ...party, myParty: newMyParty })}`);
+  }
+  const setOpponentParty = (newOpponentParty: OpponentParty) => {
+    history.push(`/?party=${JSON.stringify({ ...party, opponentParty: newOpponentParty })}`);
+  }
+
   return (
     <div className="App">
-      <MyPartyEditor value={myParty} onChange={setMyParty} />
-      <OpponentPartyEditor value={opponentParty} onChange={setOpponentParty} />
+      <MyPartyEditor value={party.myParty} onChange={setMyParty} />
+      <OpponentPartyEditor value={party.opponentParty} onChange={setOpponentParty} />
     </div>
   );
 }
