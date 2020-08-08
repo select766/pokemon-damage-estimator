a81069dd64a624a392c1e1df12c6d5967f0899d7
select766 <select766@outlook.jp>
2020-08-08 16:26:28 +0900

    Updates

---
+++ precache-manifest.cba27d7fd11a0615fb2c28a581f4e8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d0db3ec4f68b57b44d3feb46a1164f",
+    "url": "/pokemon-damage-estimator/index.html"
+  },
+  {
+    "revision": "509194ef51f763151429",
+    "url": "/pokemon-damage-estimator/static/css/main.10405320.chunk.css"
+  },
+  {
+    "revision": "b8cbf6b76d6ea95266ac",
+    "url": "/pokemon-damage-estimator/static/js/2.74559b4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-damage-estimator/static/js/2.74559b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509194ef51f763151429",
+    "url": "/pokemon-damage-estimator/static/js/main.bf2e44a9.chunk.js"
+  },
+  {
+    "revision": "7ea46d711d98defe0693",
+    "url": "/pokemon-damage-estimator/static/js/runtime-main.059fe58b.js"
+  }
+]);
